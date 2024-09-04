5e7ece3ac8e85c5c1353fb354c9e75920705598c
Shuai Chen <shuai.chen@illumio.com>
2024-09-04 22:19:50 +0900

    move cd to the top of the code

--- cleanup.sh
+++ cleanup.sh
@@ -3,9 +3,11 @@
 # 等待10秒，确保文件已生成并未被占用
 sleep 10
 
+cd /root/csv_filter/script_mbs
+
 # 查看 input 和 output 文件的大小
 echo "Checking file sizes and line counts before deletion:"
-for file in /root/csv_filter/programs/input*.csv /root/csv_filter/programs/output*.csv; do
+for file in input*.csv output*.csv; do
     if [ -f "$file" ]; then
         echo "File: $file"
         du -sh "$file"
@@ -17,7 +19,7 @@ for file in /root/csv_filter/programs/input*.csv /root/csv_filter/programs/outpu
 done
 
 # 执行删除命令，并检查是否成功
-rm -f /root/csv_filter/programs/input*.csv /root/csv_filter/programs/output*.csv
+rm -f input*.csv output*.csv
 
 # 检查命令的退出状态码
 if [ $? -eq 0 ]; then
